7592e137ea30b113d1b4735fefe063b3b2816f96
mnmandahalf <manaminakamura812@gmail.com>
2024-02-17 16:53:07 +0900

    print lines

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -1,5 +1,7 @@
-use std::error::Error;
 use clap::{App, Arg};
+use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -11,7 +13,29 @@ pub struct Config {
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    dbg!(config);
+	for filename in config.files {
+		match open(&filename) {
+			Err(err) => eprintln!("Failed to open {}: {}", filename, err),
+			Ok(file) => {
+				let mut last_num = 0;
+				for (line_num, line) in file.lines().enumerate()	 {
+					let line = line?;
+					if config.number_lines {
+						println!("{:>6}\t{}", line_num + 1, line);
+					} else if config.number_nonblank_lines {
+						if !line.is_empty() {
+							last_num += 1;
+							println!("{:>6}\t{}", last_num, line);
+						} else {
+							println!();
+						}
+					} else {
+						println!("{}", line);
+					}
+				}
+			},
+		}
+	}
     Ok(())
 }
 
@@ -48,4 +72,11 @@ pub fn get_args() -> MyResult<Config> {
       number_lines: matches.is_present("number"),
       number_nonblank_lines: matches.is_present("number_nonblank"),
     })
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+	match filename {
+		"-" => Ok(Box::new(BufReader::new(io::stdin()))),
+		_ => Ok(Box::new(BufReader::new(File::open(filename)?)))
+	}
 }
